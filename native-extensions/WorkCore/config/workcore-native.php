e8c6aa1bb20d287a5365914127541570023686a2
IOSONOSOI <masterlee.aus@gmail.com>
2026-08-03 20:19:17 +1000

    fix: declare Passport middleware for native WorkCore

--- native-extensions/WorkCore/config/workcore-native.php
+++ native-extensions/WorkCore/config/workcore-native.php
@@ -4,4 +4,10 @@ return [
     'enabled' => env('WORKCORE_NATIVE_ENABLED', true),
     'uninstall_policy' => 'retain',
     'runtime_namespace' => 'App\\Domains\\WorkCore',
+    'api_middleware' => [
+        'api',
+        env('WORKCORE_NATIVE_AUTH_MIDDLEWARE', 'auth:api'),
+        'workcore.tenant',
+        'workcore.api',
+    ],
 ];
